fb9b0ba0f50693e0e4199d88bfeb41c485d3c649
長岡聡太 <>
2025-03-08 16:42:23 +0900

    initial commit

---
+++ src/main/java/com/akitsulab/soto_account/SotoAccountApplication.java
@@ -0,0 +1,13 @@
+package com.akitsulab.soto_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotoAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotoAccountApplication.class, args);
+	}
+
+}
